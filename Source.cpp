84713103606f8a58a452be2d2580a7451731cd79
группы стационара <st@itstep.lan>
2020-11-24 17:16:40 +0300

    write TEXT file

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,14 @@
 //my_fyle.txt
 #include<iostream>
 #include<fstream>
+#include<string>
 using namespace std;
 int main() {
+	ofstream outFile;
+	outFile.open("my_file.txt");
+	string str;
+	getline(cin, str);
+	outFile << str;
+	outFile.close();
 	return 0;
 }
